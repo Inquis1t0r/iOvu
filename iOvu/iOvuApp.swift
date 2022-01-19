a0b082d9a80af22f75b525cf4ef5686c48010b62
Piotr Kazimierski <inquis1t0r@air-piotr.home>
2022-01-19 17:58:53 +0100

    initial setup

---
+++ iOvu/iOvuApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOvuApp.swift
+//  iOvu
+//
+//  Created by Piotr Kazimierski on 19/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOvuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
